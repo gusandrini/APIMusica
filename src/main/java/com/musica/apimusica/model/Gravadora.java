2207de23c94cac2cc0330375c583cea69b0968dc
gusandrini <gustavosandrini92@gmail.com>
2025-04-09 10:36:53 -0300

    adicionado genero e gravadora

---
+++ src/main/java/com/musica/apimusica/model/Gravadora.java
@@ -0,0 +1,16 @@
+package com.musica.apimusica.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Gravadora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+}
+
